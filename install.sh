dbe6ca8ccebf60f96dcee4c7f4b20e74c649e8cc
Grigory Avdyushin <avdyushin.g@gmail.com>
2022-09-05 16:43:24 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+symlink() {
+    local source=$PWD/$1
+    local target=$HOME/$1
+    if [[ -f $target || -d $target ]]; then
+        read -p "$target already exists. Overwrite it (y/n)?" override
+        case ${override:0:1} in
+            y|Y)
+                if [[ ! $target -ef "/" || ! $target -ef $HOME ]]; then
+                    rm $target
+                fi
+                ;;
+            *)
+                echo "[-] $target"
+                echo
+                continue
+                ;;
+        esac
+    fi
+    echo "[+] $source -> $target"
+    echo
+    ln -s $source $target
+}
+
+for f in .*
+do
+    case $f in
+        .|..|.git|.gitconfig|.gitignore) continue;;
+        *) symlink $f;;
+    esac
+done

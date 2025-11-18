e3c78250a78c3ed1b75168e305202bc4a1c2e2aa
Grigory Avdyushin <avdyushin.g@gmail.com>
2025-11-18 11:03:43 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,15 +4,20 @@ end
 
 source $HOME/.zshenv
 
-abbr rm 'rm -i'
-abbr mv 'mv -i'
-abbr grep 'grep --color=always'
+abbr --add rm 'rm -i'
+abbr --add mv 'mv -i'
+abbr --add grep 'grep --color=always'
 
-abbr --add l la
-abbr --add gg grep -rniEIR
-abbr --add iousb ioreg -p IOUSB
-abbr --add less less --no-histdups
+abbr --add l 'la'
+abbr --add gg --set-cursor 'grep -rniEIR \'%\''
+abbr --add less 'less --no-histdups'
 
-abbr --add L --position anywhere --set-cursor "% | less -R"
+switch (uname)
+    case Darwin
+        abbr --add iousb 'ioreg -p IOUSB'
+end
+
+
+abbr --add L --position anywhere --set-cursor '% | less -R'
 abbr --add G --position anywhere --set-cursor '% | grep'
 abbr --add '>' --position anywhere '>?'
